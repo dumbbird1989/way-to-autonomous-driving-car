3331fc5a94190e5a842e3cede419201b4ed450ee
zrx <zrx_2428@163.com>
2018-07-17 21:37:03 +0800

    kalman filter markdown and some code in cpp

---
+++ kalman-filter/build/CMakeFiles/kalman-filter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kalman-filter.dir/kalman-filter.cc.o"
+  "kalman-filter.pdb"
+  "kalman-filter"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kalman-filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- kalman-filter/kalman-filter.cc
+++ kalman-filter/kalman-filter.cc
@@ -1,8 +1,26 @@
+/*
+For kalman filter parameters:
+x[n] is the state parameters, in prediction update, we predict x with the "real" state at time t-1 which is x(t-1)
+and in measurement update. we calculate "real"  state at time x which is x(t)
+
+z[n] is the state measurement value, some values may not be measured such as velocity, so some states is zero
+
+
+*/
+
 #include <Eigen/Dense>
 #include <iostream>
 
-MatrixXf x(1,2);
-MatrixXf P(2,2);
+using namespace std;
+using namespace Eigen;
+
+MatrixXf x(2,1); // initial state(location and velocity)
+MatrixXf P(2,2); // initial uncertainty
+MatrixXf u(2,1); // external motion (control)
+MatrixXf F(2,2); // next state function
+MatrixXf H(1,2); // measurement function
+MatrixXf R(1,1); //measurement uncertainty
+MatrixXf I = MatrixXf::Identity(2,2); //identity matrix
 
 void initial_parameter(void)
 {
@@ -10,10 +28,38 @@ void initial_parameter(void)
           0		, 1000.0;
     x << 0, 0;
 
-    std::cout<< "initial state: position and velocity "<<P<<std::endl;
+    u << 0,0;
+
+    F << 1,1,0,1;
+
+    H << 1, 0;
+
+    R << 1;
+
+
+    std::cout<< "initial state: position is "<< x(0,0) <<" velocity is "<<x(1,0)<<std::endl;
 
     std::cout<< "initial covariancematrix: "<< P << std::endl << std::endl;
 
+    std::cout<< "external motion is "<<u<<std::endl;
+
+    std::cout<< "next state function is "<<F <<std::endl<<std::endl;
+
+    std::cout<< "measurement function is "<< H << std::endl;
+
+    std::cout<< "measurement uncertainty is "<<R << std::endl;
+
 }
 
-void kalman_filter(double x_initial, )
+void kalman_filter(double measurement)
+{
+	/*step1 measurement update*/
+
+	Eigen::MatrixXf Z;
+}
+
+int main(void)
+{
+	initial_parameter();
+	return 1;
+}
